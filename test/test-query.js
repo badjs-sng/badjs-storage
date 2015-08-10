b1974324bd22f777fff88de22802740d4c796366
homker <liu15jia@163.com>
2015-08-10 09:34:35 +0800

    添加测试

--- test/test-query.js
+++ test/test-query.js
@@ -8,30 +8,29 @@ var mongoUrl = "mongodb://betterjs:betterjs4imweb@localhost:27017/badjs";
 
 var mongoDB;
 // Use connect method to connect to the Server
-MongoClient.connect(mongoUrl, function(err, db) {
-    if(err){
+MongoClient.connect(mongoUrl, function (err, db) {
+    if (err) {
         console.log("failed connect to server");
-        return ;
-    }else {
+        return;
+    } else {
         console.log("Connected correctly to server");
     }
     mongoDB = db;
 
 
-
     var queryJSON = {};
+    // lt endTime     gt startTime
+    queryJSON.date = {$lt: new Date(1439170351000), $gt: new Date(1439166840000)};
 
-    queryJSON.date = {$lt : new Date(1418794477352) , $gt : new Date (1418708077352) };
-
-    queryJSON.level = {$all : [4] } ;
+    queryJSON.level = {$all: [4]};
 
     var startTime = Date.now();
-    mongoDB.collection('badjslog_' + 51).find(queryJSON , function (error,cursor){
-        console.log(error)
-        cursor.sort({'date' : -1}).skip(0).limit(50000).forEach(function(item){
-          if(item){
-               console.log(JSON.stringify(item));
-           }
+    mongoDB.collection('badjslog_' + 51).find(queryJSON, function (error, cursor) {
+        console.log(error);
+        cursor.sort({'date': -1}).skip(0).limit(50000).forEach(function (item) {
+            if (item) {
+                console.log(JSON.stringify(item));
+            }
         });
 
     });
